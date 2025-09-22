ba830c6af98969b8fc9ca92cc67aec7e5ccd0644
Shashank Sapre <shashank.sapre@outlook.com>
2025-09-22 19:59:55 +0530

    lint security

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig, globalIgnores } from "eslint/config";
+import pluginSecurity from "eslint-plugin-security";
 
 export default defineConfig([
   {
@@ -11,6 +12,7 @@ export default defineConfig([
     languageOptions: { globals: globals.browser },
   },
   tseslint.configs.recommended,
+  pluginSecurity.configs.recommended,
   globalIgnores([
     "dist/**/*", // ignore all contents in and under `build/` directory but not the `build/` directory itself
   ]),
